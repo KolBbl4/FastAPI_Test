b62f7392e580664eba2747cff2da711d2059507f
KolBbl4 <ko1ba@mail.ru>
2024-08-17 20:19:59 +0500

    test post

--- main.py
+++ main.py
@@ -1,4 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from typing import Optional
 from datetime import date
 from pydantic import BaseModel
+
+app = FastAPI()
+
+class HostParams:
+    def __init__(self,
+                 localtion: str = None,
+                 datFrom: str = None,
+
+                 ) -> None:
+        self.localtion = localtion
+        self.datFrom = datFrom
+        
+
+@app.get("/host")
+def getHost(serhArg: HostParams = Depends()
+            ):
+    return serhArg
+
+class SModelParam (BaseModel):
+    id:int
+    value: str
+
+app.post("/host")
+def setParam (param:SModelParam):
+    pass
